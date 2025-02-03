16f1384fcc700f6c092990ee19ee4002b8a02db4
vale46tetsu <vale46tetsu@gmail.com>
2025-02-03 22:11:55 +0900

    課題を提出

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,17 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+	
+	public void setGivenName() {
+		
+		this.givenName = "花子";
+		
+	};
+	
+	public void eachIntroduce() {
+		
+		System.out.println("好きな食べ物はリンゴです");
+		
+	};
+
+};
---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,17 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+	
+	public void setGivenName() {
+		
+		this.givenName = "一郎";
+		
+	};
+	
+	public void eachIntroduce() {
+		
+		System.out.println("趣味は読書です");
+		
+	};
+
+};
